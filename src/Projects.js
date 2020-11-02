c1298210e575d763d83627184dc51b71946c9637
JP Kelly <jp@jpkvideo.com>
2020-11-01 20:30:18 -0800

    commit

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Thumbnail from './Thumbnail.js';
+import './App.css';
 
 function Projects(props) {
   return (
     <div>
       <h1>Projects</h1>
+      <Thumbnail
+        link="https://via.placeholder.com/150"
+        image="https://via.placeholder.com/150"
+        title="150 px"
+        category=""
+      />
+      <Thumbnail
+        link="https://via.placeholder.com/150"
+        image="https://via.placeholder.com/150"
+        title="150 px"
+        category=""
+      />
+      <Thumbnail
+        link="https://via.placeholder.com/150"
+        image="https://via.placeholder.com/150"
+        title="150 px"
+        category=""
+      />
     </div>
   );
 }

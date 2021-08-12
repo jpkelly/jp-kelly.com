f670a4b6f501875bb8aa248f6bda4e68658f2132
JP Kelly <jp@jpkvideo.com>
2021-08-12 12:46:32 -0700

    responsive header tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,13 @@ const Header = props => {
     <h1 className="xl:text-3xl 2xl:text-4xl">
       <Link to="/">
         {/* <kamon className="inline-block align-top" /> */}
-        <img src={kamon} className="inline-block align-top kamon" alt="logo" />
+        <img
+          src={kamon}
+          className="inline-block align-top kamon mx-0 my-0 transform scale-50 xl:scale-75 2xl:scale-100 -mr-2 xl:mr-2 2xl:mr-4"
+          alt="logo"
+        />
       </Link>
-      <div className="inline-block mt-1 divide-x-2">
+      <div className="inline-block mt-2.5 xl:mt-1 divide-x-2">
         <Link className="px-3" to="/">
           Projects
         </Link>

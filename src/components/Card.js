3ec59cedf9915b6df6ac46da9362a324da0117f0
JP Kelly <jp@jpkvideo.com>
2021-12-23 20:03:12 -0800

    fix column breaks

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,22 +5,20 @@ import ImageList from './ImageList';
 
 const Card = ({ title, text, imgsrc, link }) => {
   return (
-    // <div className=" my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-    <div className=" break-inside mb-8">
-      <a href={link}>
-        <article className="group overflow-hidden border border-gray-300 rounded-lg">
-          <header className=" items-center justify-between leading-tight p-2 md:p-4">
-            <h3 className="text-lg">
-              <text className="no-underline ">{title}</text>
-            </h3>
-            <p className="text-sm">{text}</p>
-          </header>
-          <a href={link}>
-            {/* <img alt={alttext} className="block h-auto w-full" src={imgsrc} /> */}
+    <div className="break-inside">
+      <div className="pb-8">
+        <a href={link}>
+          <article className="group overflow-hidden border border-gray-300 rounded-lg">
+            <header className=" items-center justify-between leading-tight p-2 md:p-4">
+              <h3 className="text-lg">
+                <text className="no-underline ">{title}</text>
+              </h3>
+              <p className="text-sm">{text}</p>
+            </header>
             <ImageList photo={imgsrc} />
-          </a>
-        </article>
-      </a>
+          </article>
+        </a>
+      </div>
     </div>
   );
 };

572e6ce8174c78558c17f4b28c18beea48f48265
JP Kelly <jp@jpkvideo.com>
2023-07-12 17:36:36 -0700

    fix autoplay NACVJ

--- src/components/NAC23VJ.js
+++ src/components/NAC23VJ.js
@@ -7,7 +7,7 @@ function NAC23VJ(props) {
 			<div className="container mx-auto">
 				<h2>NAC23 VJ Rig</h2>
 				<p>VJ rig built in TouchDesigner. Thank you to Satoriteller and Experience Engineers for inviting me along!</p>
-				<Vimeo className="z-0" video={844771292} width={1280} height={720} autopause="True" responsive="True" controls="False" />
+				<Vimeo className="z-0" video={844771292} width={1280} height={720} autopause="False" responsive="True" controls="False" />
 			</div>
 		</div>
 	);

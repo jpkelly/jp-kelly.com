5f364e3af638237468cddb4f76762b93d0c7f2c9
JP Kelly <jp_kelly@mac.com>
2026-03-21 16:56:34 -0700

    Fix gallery thumbnail URLs and logo sizing

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,20 +2,19 @@ import React from 'react';
 
 class ImageList extends React.Component {
   render() {
-    let images = [];
     const { photo } = this.props;
     const lastIndex = photo.length - 1;
-    photo.map((p, i) => {
+    return photo.map((p, i) => {
+      const normalizedSrc = /^(https?:)?\/\//i.test(p.src) || p.src.startsWith('/') ? p.src : `/${p.src}`;
+
       if (i === lastIndex) {
         // last one
-        images.push(<img alt={p.alt} className="block h-auto w-full" src={p.src} />);
+        return <img key={`${p.src}-${i}`} alt={p.alt} className="block h-auto w-full" src={normalizedSrc} />;
       } else {
         // not last one
-        images.push(<img alt={p.alt} className="block h-auto w-full mb-1" src={p.src} />);
+        return <img key={`${p.src}-${i}`} alt={p.alt} className="block h-auto w-full mb-1" src={normalizedSrc} />;
       }
-      return true;
     });
-    return images;
   }
 }
 

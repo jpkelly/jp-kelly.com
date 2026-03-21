736738ef0c8380bf2221e06b7cdf4508a80b8bba
JP Kelly <jp_kelly@mac.com>
2026-03-21 14:59:57 -0700

    VimeoEmbed: replace react-vimeo with direct iframe embed

--- src/components/mdx/VimeoEmbed.js
+++ src/components/mdx/VimeoEmbed.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Vimeo from '@u-wave/react-vimeo';
 
 function VimeoEmbed({
   video,
@@ -9,20 +8,23 @@ function VimeoEmbed({
   muted = false,
   background = false
 }) {
+  const params = new URLSearchParams({ autopause: 0 });
+  if (autoplay) params.set('autoplay', 1);
+  if (muted) params.set('muted', 1);
+  if (loop) params.set('loop', 1);
+  if (!controls) params.set('controls', 0);
+  if (background) params.set('background', 1);
+
   return (
-    <Vimeo
-      className="z-0"
-      video={video}
-      width={1280}
-      height={720}
-      responsive
-      autopause={false}
-      controls={controls}
-      autoplay={autoplay}
-      loop={loop}
-      muted={muted}
-      background={background}
-    />
+    <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+      <iframe
+        src={`https://player.vimeo.com/video/${video}?${params}`}
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+        title={`Vimeo video ${video}`}
+      />
+    </div>
   );
 }
 

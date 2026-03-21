9e9fad8042b0ffe4f3c48be3895d418754691050
JP Kelly <jp_kelly@mac.com>
2026-03-21 12:28:10 -0700

    Migrate site build from CRA to Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement(React.StrictMode, null, React.createElement(App)),
   document.getElementById('root')
 );
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  envPrefix: ['VITE_', 'REACT_APP_'],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
+  build: {
+    outDir: 'build',
+    emptyOutDir: true
+  }
+});
